34b4f3cd79a4755d40561aa2349eb737188b8ee8
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-13 20:24:09 +0800

    feat: convert to ollama tools

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module mcptry
 
 go 1.24.6
 
-require github.com/modelcontextprotocol/go-sdk v0.2.0
+require (
+	github.com/modelcontextprotocol/go-sdk v0.2.0
+	github.com/ollama/ollama v0.11.4
+	github.com/siuyin/dflt v0.0.0-20230329062002-0475f4d54412
+)
 
 require (
-	github.com/siuyin/dflt v0.0.0-20230329062002-0475f4d54412 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
---
+++ ollamaTools/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+	"github.com/ollama/ollama/api"
+	"github.com/siuyin/dflt"
+)
+
+func main() {
+	name := dflt.EnvString("NAME", "Siu Yin")
+	svr := dflt.EnvString("SERVER", "myserver")
+	log.Printf("SERVER=%s NAME=%s", svr, name)
+
+	ctx := context.Background()
+
+	// Create a new client, with no features.
+	client := mcp.NewClient(&mcp.Implementation{Name: "mcp-client", Version: "v1.0.0"}, nil)
+
+	// Connect to a server over stdin/stdout
+	transport := mcp.NewCommandTransport(exec.Command(svr))
+	session, err := client.Connect(ctx, transport)
+	if err != nil {
+		log.Fatal("connect: ", err)
+	}
+	defer session.Close()
+
+	// List Tools
+	lt, err := session.ListTools(ctx, &mcp.ListToolsParams{})
+	if err != nil {
+		log.Fatal("list tools: ", err)
+	}
+	for _, t := range lt.Tools {
+		is := t.InputSchema
+		nameProp := is.Properties["name"]
+		fmt.Printf("%s: %v %v %#v\n", t.Name, t.InputSchema.Type, t.InputSchema.Required, nameProp.Type)
+	}
+
+	ollamaTools(lt)
+
+	// Call a tool on the server.
+	params := &mcp.CallToolParams{
+		Name:      "greet",
+		Arguments: map[string]any{"name": name},
+	}
+	res, err := session.CallTool(ctx, params)
+	if err != nil {
+		log.Fatalf("CallTool failed: %v", err)
+	}
+	if res.IsError {
+		log.Fatal("tool failed")
+	}
+	for _, c := range res.Content {
+		log.Print(c.(*mcp.TextContent).Text)
+	}
+}
+func ollamaTools(lt *mcp.ListToolsResult) {
+	tools, _ := ConvertMCPToolsToOllamaTools(lt.Tools)
+	fmt.Printf("%v\n", tools)
+}
+
+func ConvertMCPToolsToOllamaTools(mcpTools []*mcp.Tool) ([]api.Tool, error) {
+	ollamaTools := make([]api.Tool, len(mcpTools))
+	for i, tool := range mcpTools {
+		// Convert mcp.ToolProperty to the required JSON schema format for Ollama.
+		properties := make(map[string]api.ToolProperty)
+		required := []string{}
+		required = append(required, tool.InputSchema.Required...)
+		for propName, prop := range tool.InputSchema.Properties {
+			properties[propName] = api.ToolProperty{
+				Type:        api.PropertyType{prop.Type},
+				Description: prop.Description,
+			}
+		}
+
+		// Marshal the parameters into the format expected by the Ollama API
+		ollamaTools[i] = api.Tool{
+			Type: "function",
+			Function: api.ToolFunction{
+				Name:        tool.Name,
+				Description: tool.Description,
+				Parameters: struct {
+					Type       string                      `json:"type"`
+					Defs       any                         `json:"$defs,omitempty"`
+					Items      any                         `json:"items,omitempty"`
+					Required   []string                    `json:"required"`
+					Properties map[string]api.ToolProperty `json:"properties"`
+				}{
+					Type:       "object",
+					Properties: properties,
+					Required:   required,
+				},
+			},
+		}
+	}
+	return ollamaTools, nil
+}
