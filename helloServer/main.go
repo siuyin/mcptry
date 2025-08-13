7b1ffeee35bc8f96d155a722c37e5e024cecbb5d
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-13 20:48:04 +0800

    feat: add bye mcp tool

--- helloServer/main.go
+++ helloServer/main.go
@@ -7,13 +7,19 @@ import (
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
-type HiParams struct {
-	Name string `json:"name" jsonschema:"the name of the person to greet"`
+type NameParams struct {
+	Name string `json:"name" jsonschema:"the name of the person to address"`
 }
 
-func SayHi(ctx context.Context, ss *mcp.ServerSession, req *mcp.CallToolParamsFor[HiParams]) (*mcp.CallToolResultFor[any], error) {
+func SayHi(ctx context.Context, ss *mcp.ServerSession, req *mcp.CallToolParamsFor[NameParams]) (*mcp.CallToolResultFor[any], error) {
 	return &mcp.CallToolResultFor[any]{
 		Content: []mcp.Content{&mcp.TextContent{Text: "Hi " + req.Arguments.Name}},
+	}, nil
+}
+
+func SayBye(ctx context.Context, ss *mcp.ServerSession, req *mcp.CallToolParamsFor[NameParams]) (*mcp.CallToolResultFor[any], error) {
+	return &mcp.CallToolResultFor[any]{
+		Content: []mcp.Content{&mcp.TextContent{Text: "Goodbye " + req.Arguments.Name}},
 	}, nil
 }
 
@@ -23,6 +29,7 @@ func main() {
 	server := mcp.NewServer(&mcp.Implementation{Name: "greeter", Version: "v1.0.0"}, nil)
 
 	mcp.AddTool(server, &mcp.Tool{Name: "greet", Description: "say hi"}, SayHi)
+	mcp.AddTool(server, &mcp.Tool{Name: "bye", Description: "say goodbye"}, SayBye)
 	// Run the server over stdin/stdout, until the client disconnects
 	if err := server.Run(context.Background(), mcp.NewStdioTransport()); err != nil {
 		log.Println("run: ", err)
